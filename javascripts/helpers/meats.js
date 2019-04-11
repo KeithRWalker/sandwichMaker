cba29d817a41977654e6e3b62af74f738e89da24
Keith Walker <Keithrwalker418@gmail.com>
2019-04-10 20:26:45 -0500

    added ingredient modules

---
+++ javascripts/helpers/meats.js
@@ -0,0 +1,14 @@
+const meatInfo = {
+    turkey: 3.00,
+    ham: 3.00,
+    chicken: 4.00,
+    beef: 4.00,
+    steak: 5.00,
+    noMeat: 0
+};
+
+const getMeat = () => {
+    return meatInfo;
+};
+
+export default { getMeat };
