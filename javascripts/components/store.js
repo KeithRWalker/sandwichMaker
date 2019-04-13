1a49402e8ac71ece942a86e1f8515491f323729c
Keith Walker <Keithrwalker418@gmail.com>
2019-04-13 11:33:56 -0500

    changed ing mods names for consistency

---
+++ javascripts/components/store.js
@@ -0,0 +1,7 @@
+import util from `../helpers/util.js`;
+import getBread from `../helpers/bread`;
+import getCheese from `../helpers/cheese`;
+import getCondiment from `../helpers/condiment`;
+import getMeat from `../helpers/meat`;
+import getMeat from `../helpers/veggie`;
+
